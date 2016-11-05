8c39369bcc54dd1901b27142500ad8af17cbcf02
hugheskatie <khughes@appnexus.com>
2016-11-04 19:59:49 -0700

    Elm tutorial progress

--- elm/elm-tutorial/simple_project/src/Main.elm
+++ elm/elm-tutorial/simple_project/src/Main.elm
@@ -1,27 +1,42 @@
 module Main exposing (..)
 
-import Html.App
-import Messages exposing (Msg)
-import Models exposing (Model)
+import Navigation
+import Messages exposing (Msg(..))
+import Models exposing (Model, initialModel)
 import View exposing (view)
 import Update exposing (update)
+import Players.Commands exposing (fetchAll)
+import Routing exposing (Route)
 
 
-init : ( Model, Cmd Msg )
-init =
-  ( "Hello", Cmd.none )
+init : Result String Route -> ( Model, Cmd Msg )
+init result =
+  let
+      currentRoute =
+        Routing.routeFromResult result
+  in
+    ( initialModel currentRoute, Cmd.map PlayersMsg fetchAll )
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
 
+urlUpdate : Result String Route -> Model -> ( Model, Cmd Msg )
+urlUpdate result model =
+  let
+      currentRoute =
+        Routing.routeFromResult result
+  in
+     ( { model | route = currentRoute }, Cmd.none )
+
 
 -- Main
 main : Program Never
 main =
-  Html.App.program
+  Navigation.program Routing.parser
         { init = init
         , view = view
         , update = update
+        , urlUpdate = urlUpdate
         , subscriptions = subscriptions
         }
